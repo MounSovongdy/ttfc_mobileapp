4ac32afecc2de2b1b8d9db2a0372fca228b64c4a
MounSovongdy <78632524+MounSovongdy@users.noreply.github.com>
2024-05-03 16:22:54 +0700

    test

---
+++ android/app/src/main/kotlin/com/example/ttfc_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ttfc_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utility/constant.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const defaultPadding = 8.0;
+const fonttitle = 24.0;
+const fonttext = 18.0;
+const fontdiscription = 14.0;
+const colorBackground = 0.4;
+const borderCircle = 10.0;
+const dailogBackground = 0.9;
+const iconSize = 32.0;
+
+const bgColor = Color(0xFF030BA8);
+const primaryColor = Color(0xFFD4AF37);
+const secondColor = Color(0xFF2E325C);
+const blackColor = Color(0xFF000000);
+const whiteColor = Color(0xFFFFFFFF);
+const primaryGrayColor = Color(0xFFEEEEEE);
+const secondGraydColor = Color(0xFFAAAAAA);
+const noneColor = Colors.transparent;
+const acceptColor = Color(0xFF76FF03);
+const rejectColor = Color(0xFFD50000);
